7085290482e5c0bf926cb39e6945e2769d15c9a3
PSDCenter <bpekarek@psdcenter.com>
2018-01-02 15:29:05 +0530

    Equal height script

---
+++ equalheight.js
@@ -0,0 +1,23 @@
+function equalheight(container) {
+  var currentTallest = 0, currentRowStart = 0, rowDivs = new Array(), $el, topPosition = 0;
+  $(container).each(function() {
+    $el = $(this);
+    $($el).height('auto')
+    var topPostion = $el.position().top;
+    if (currentRowStart != topPostion) {
+      for (var currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        rowDivs[currentDiv].height(currentTallest);
+      }
+      rowDivs.length = 0;
+      currentRowStart = topPostion;
+      currentTallest = $el.height();
+      rowDivs.push($el);
+    } else {
+      rowDivs.push($el);
+      currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+    }
+    for (var currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+      rowDivs[currentDiv].height(currentTallest);
+    }
+  });
+}
